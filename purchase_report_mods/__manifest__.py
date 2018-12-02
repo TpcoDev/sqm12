6e80e6898b920c613e261ded7594c3e51e0aeb1f
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2018-12-02 15:06:07 -0300

    purchase_report_mods added.

---
+++ purchase_report_mods/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Product Adittional Fields module for Odoo
+#    Copyright (C) 2018 The Project Comany (www.tpco.cl)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Purchase Report Mods',
+    'version': '12.0',
+    'category': 'Purchase',
+    'license': 'AGPL-3',
+    'summary': 'Modificaciones a Purchase Reports',
+    'author': 'The Project Company',
+    'website': 'www.tpco.cl',
+    'depends': ['purchase'],
+    'data': [
+        "views/purchase_report_view.xml"
+    ],
+    'installable': True,
+}
