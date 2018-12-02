9381a8c7b25de55945b290078220e2d1a5e7ee7b
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2018-12-02 15:09:58 -0300

    purchase_report_mods updated.

--- purchase_report_mods/reports/purchase_report.py
+++ purchase_report_mods/reports/purchase_report.py
@@ -28,7 +28,7 @@ class PurchaseReport(models.Model):
                     l.x_studio_dias_de_atraso,
                     l.x_studio_estado,
                     l.x_studio_estado_de_los_das,
-                    l.x_studio_posicion_sap,
+                    l.x_studio_posicin_sap as x_studio_posicion_sap,
                     s.date_order as date_order,
                     s.state,
                     s.date_approve,
