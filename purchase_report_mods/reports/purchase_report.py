ad927a032987a592523571f593c6ffd344e24db6
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2018-12-03 03:27:11 -0300

    purchase_report_mods updated.

--- purchase_report_mods/reports/purchase_report.py
+++ purchase_report_mods/reports/purchase_report.py
@@ -17,7 +17,8 @@ class PurchaseOrderLine(models.Model):
         if 'x_studio_comentarios' in vals:
             super(PurchaseOrderLine, self).write(vals)
             record = self.search([('id', '=', self.id)])
-            vals.update({'x_studio_hist_comentarios': fields.datetime.now(tz = pytz.timezone(self.env.user.partner_id.tz)).strftime('%d-%m-%Y %H:%M:%S') + ": " + vals.get('x_studio_comentarios') + "\n\n" if len(record.x_studio_hist_comentarios) > 0 + record.x_studio_hist_comentarios})
+            salto = "\n\n" if len(record.x_studio_hist_comentarios) > 0 else ""
+            vals.update({'x_studio_hist_comentarios': fields.datetime.now(tz = pytz.timezone(self.env.user.partner_id.tz)).strftime('%d-%m-%Y %H:%M:%S') + ": " + vals.get('x_studio_comentarios') + salto + record.x_studio_hist_comentarios})
         return super(PurchaseOrderLine, self).write(vals)
 
 class PurchaseReport(models.Model):
