c0639701cf9b47110a195bf937e2bf62f114998a
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2018-12-03 02:42:03 -0300

    purchase_report_mods updated.

--- purchase_report_mods/reports/purchase_report.py
+++ purchase_report_mods/reports/purchase_report.py
@@ -12,6 +12,14 @@ class PurchaseOrderLine(models.Model):
     _inherit = ["purchase.order.line", 'mail.thread', 'mail.activity.mixin', 'portal.mixin']
     _name = "purchase.order.line"
 
+    @api.multi
+    def write(self, vals):
+        if 'x_studio_comentarios' in vals:
+            super(PurchaseOrderLine, self).write(vals)
+            record = self.search([('id', '=', self.id)])
+            vals.update({'x_studio_hist_comentarios': vals.get('x_studio_comentarios', '') + "\n\n" + record.x_studio_hist_comentarios})
+        return super(PurchaseOrderLine, self).write(vals)
+
 class PurchaseReport(models.Model):
     _inherit = ["purchase.report"]
 
