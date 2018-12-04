4d83ff4fef26e102774b4167813a76e137345c53
rockcesar <rockcesar@gmail.com>
2018-12-03 21:11:14 -0300

    Update purchase_report.py

--- purchase_report_mods/reports/purchase_report.py
+++ purchase_report_mods/reports/purchase_report.py
@@ -19,7 +19,7 @@ class PurchaseOrderLine(models.Model):
         if 'x_studio_comentarios' in vals:
             super(PurchaseOrderLine, self).write(vals)
             record = self.search([('id', '=', self.id)])
-            salto = "\n\n" if len(record.x_studio_hist_comentarios) > 0 else ""
+            salto = "\n\n" if record.x_studio_hist_comentarios and len(record.x_studio_hist_comentarios) > 0 else ""
             vals.update({'x_studio_hist_comentarios': textile.textile("<strong>" + fields.datetime.now(tz = pytz.timezone(self.env.user.partner_id.tz)).strftime('%d-%m-%Y %H:%M:%S') + " (" + self.env.user.partner_id.name + ")" + ":</strong> " + vals.get('x_studio_comentarios', '') + salto) + record.x_studio_hist_comentarios})
         return super(PurchaseOrderLine, self).write(vals)
     
